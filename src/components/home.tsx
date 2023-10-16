c0b980a4110a4ed7875db97a21e3282ba4ef39d5
Jeremy Moritz <jeremy@jeremymoritz.com>
2023-10-15 22:59:40 -0500

    add images and styling to home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,6 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { Button } from 'react-native-paper';
 
 function Home({ onMoveOnClick }: { onMoveOnClick(): void }): JSX.Element {
@@ -8,15 +15,32 @@ function Home({ onMoveOnClick }: { onMoveOnClick(): void }): JSX.Element {
     <SafeAreaView>
       <ScrollView>
         <View>
-          <Text>Rivalry Club is cool!</Text>
+          <Text>Rivalry Club</Text>
           <Button mode="contained" onPress={onMoveOnClick}>
             <FontAwesomeIcon icon="square-check" /> Click here to continue!{' '}
             <FontAwesomeIcon icon="stroopwafel" />
           </Button>
+          <Image
+            style={styles.logoImage}
+            source={{
+              uri: 'https://rivalry-club.s3.amazonaws.com/images/logos/super_smash_bros_ultimate.png',
+            }}
+          />
+          <Image
+            style={styles.fighterImage}
+            source={{
+              uri: 'https://rivalry-club.s3.amazonaws.com/images/fighters/ssbu/_large/banjo__kazooie.png',
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  logoImage: { height: 250 },
+  fighterImage: { height: 460 },
+});
 
 export default Home;
